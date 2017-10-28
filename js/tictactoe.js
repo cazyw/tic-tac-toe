033c2deb15f7c29036be2f8f767196f5d41d99fb
cazyw <cazyw@users.noreply.github.com>
2017-10-28 14:34:46 +1100

    Complete refactor of game code structure

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -2,7 +2,6 @@
 var timer
 var tictactoe
 
-
 class Game {
   constructor() {
     this.players = {
@@ -13,7 +12,6 @@ class Game {
     this.noPlayers = 1;
     this.playerTurn = 1;
     this.playable = false;
-    this.winner = "";
   }
 }
 
@@ -27,74 +25,43 @@ const colorWinner = (a, b, c) => {
 // check if there's a winning row/column/diagonal
 const checkWinner = (board) => {
   if (board[0] === board[1] && board[1] === board[2] && board[1] != ""){
-    console.log('row 1');
     return [0, 1, 2];
   }
   if (board[3] === board[4] && board[4] === board[5] && board[4] != ""){
-    console.log('row 2')
     return [3, 4, 5];
   }
   if (board[6] === board[7] && board[7] === board[8] && board[7] != ""){
-    console.log('row 3')
     return [6, 7, 8];
   }
   if (board[0] === board[3] && board[3] === board[6] && board[3] != ""){
-    console.log('col 1')
     return [0, 3, 6];
   }
   if (board[1] === board[4] && board[4] === board[7] && board[4] != ""){
-    console.log('col 2')
     return [1, 4, 7];
   }
   if (board[2] === board[5] && board[5] === board[8] && board[5] != ""){
-    console.log('col 3')
     return [2, 5, 8];
   }
   if (board[0] === board[4] && board[4] === board[8] && board[4] != ""){
-    console.log('diag lr')
     return [0, 4, 8];
   }
   if (board[2] === board[4] && board[4] === board[6] && board[4] != ""){
-    console.log('diag rl')
     return [2, 4, 6];
   } 
   return false;
 }
 
-
-
-// let board = ["", "", "", "", "", "", "", "", ""]
-
-// boards = [["X", "", "", "", "", "", "", "", ""],
-// ["X", "X", "", "", "", "", "", "", ""],
-// ["X", "", "X", "", "", "", "", "", ""],
-// ["X", "", "", "X", "", "", "", "", ""],
-// ["X", "", "", "", "X", "", "", "", ""],
-// ["X", "", "", "", "", "X", "", "", ""],
-// ["X", "", "", "", "", "", "X", "", ""],
-// ["X", "", "", "", "", "", "", "X", "X"],
-// ["", "X", "", "", "", "", "", "X", ""],
-// ["", "", "X", "", "", "", "X", "", ""],
-// ["", "X", "", "", "", "X", "", "", ""],
-// ["", "", "X", "", "", "", "", "", "X"],
-// ["", "", "", "X", "", "X", "", "", ""],
-// ["", "", "", "", "X", "X", "", "", ""],
-// ["", "", "", "", "X", "", "", "", ""],
-// ["O", "", "O", "", "X", "", "O", "", "O"],
-// ["O", "", "", "", "", "", "O", "", "O"],
-// ["O", "X", "", "", "O", "", "", "", "X"],
-// ["X", "O", "", "", "X", "", "", "", "O"]]
-
+// check if the opponent will win on the next move
 const checkPendingDoom = (testBoard, i, m) => {
   testBoard[i] = m;
-  console.log("checking board ", testBoard);
   if (checkWinner(testBoard)) {
     return true;
   } 
 }
 
+// check if the opponent could place a piece that 
+// produces two possible win scenarios
 const checkPendingFork = (forkBoard, m) => {
-  console.log("fork board ", forkBoard);
   let countPossWins = 0;
   for (let i = 0; i < 9; i++) {
     let testBoard = [].concat(forkBoard);
@@ -102,7 +69,6 @@ const checkPendingFork = (forkBoard, m) => {
       countPossWins += 1;
     }
   }
-  console.log("poss wins: " + countPossWins);
   return countPossWins > 1;
 }
 
@@ -114,43 +80,33 @@ const randomiseArray = (arr) => {
   return res.concat(randomiseArray(arr));
 }
 
+// computer decides which is the optimal move
 const compMove = (board) => {
   var compPiece = tictactoe.players.player2;
-  console.log("COMPUTER: ", tictactoe.players.player2);
   var playerPiece = 'X'
   compPiece === 'X' ? playerPiece = 'O' : playerPiece = 'X'; 
-  console.log('Board: ', board);
-
   // test if computer can win 
-  console.log('Checking if computer has winning piece');
-  
   for (let i = 0; i < 9; i++) {
     let testBoard = [].concat(board)
     if (testBoard[i] === '') {
       testBoard[i] = compPiece;
       if (checkWinner(testBoard)){
-        console.log('COMPUTER WINS ==> ', testBoard);
         return i;
       }
     } 
   }
 
   // test if other player could win on their next move
-  console.log('checking if player has a winning piece')
-  
   for (let i = 0; i < 9; i++) {
     let testBoard = [].concat(board)
     if (testBoard[i] === '' && checkPendingDoom(testBoard, i, playerPiece)){
-      console.log('TRUE ==> ', testBoard);
       return i;
     }
   }
   
-  console.log('checking if special case')
-  // checking for the special case
+  // checking for the special case of a diagonal fork
   v1 = [`${playerPiece}`, "", "", "", `${compPiece}`, "", "", "", `${playerPiece}`];
   v2 = ["", "", `${playerPiece}`, "", `${compPiece}`, "", `${playerPiece}`, "", ""];
-  console.log(board, v1, v2);
   let same = true;
   for (let i = 0; i < 9 ; i++) {
     if (board[i] !== v1[i]) {
@@ -159,7 +115,6 @@ const compMove = (board) => {
     }
   }
   if (same) {
-    console.log("SPECIAL")
     return randomiseArray([1, 3, 5, 7]).find(sq => board[sq] === '');
   }
   same = true;
@@ -170,58 +125,45 @@ const compMove = (board) => {
     }
   }
   if (same) {
-    console.log("SPECIAL")
     return randomiseArray([1, 3, 5, 7]).find(sq => board[sq] === '');
   }
   
-  console.log('checking if computer can fork')
   // test if we can win two moves ahead
   for (let i = 0; i < 9; i++) {
     let testBoard = [].concat(board)
     if (testBoard[i] === '') {
       testBoard[i] = compPiece;
       if (checkPendingFork(testBoard, compPiece)){
-        console.log('computer FORK ' + i);
         return i;
       }
     }
   }
   
-  console.log('checking if player can fork')
   // test if other player could win two moves ahead
   for (let i = 0; i < 9; i++) {
     let testBoard = [].concat(board)
     if (testBoard[i] === '') {
       testBoard[i] = playerPiece;
       if (checkPendingFork(testBoard, playerPiece)){
-        console.log('PENDING FORK ' + i);
         return i;
       }
     }
   }
   
-  console.log('checking center')
   // test for centre square
   if (board[4] === '') {
-    console.log('CENTER');
     return 4;
   }
   
-  console.log('checking corners')
   // test for corners
   let corner = randomiseArray([0, 2, 6, 8]).find(sq => board[sq] === '');
-  console.log(corner);
   if (corner !== undefined) {
-    console.log('CORNER ' + corner);
     return corner;
   }
   
-  console.log('checking sides')
   // test for sides
-  // [1, 3, 5, 7].findIndex(sq => sq === '')
   let side = randomiseArray([1, 3, 5, 7]).find(sq => board[sq] === '');
   if (side !== undefined) {
-    console.log('SIDE ' + side);
     return side;
   }
 }
@@ -238,22 +180,20 @@ const computerPlays = () => {
 // resets the game, doesn't reset the game object values
 // as that is reset on the first menu screen
 const resetGame = () => {
-  console.log('reset game');
-  updateScore('player1', 'reset');
-  updateScore('player2', 'reset');
+  updateScore('player1', 'reset'); // scores reset to zero
+  updateScore('player2', 'reset'); // scores reset to zero
 
-  addedClass('outcome', 'hide');
-  addedClass('turn', 'hide');
-  removedClass('intro', 'hide');
-  removedClass('playerPiece', 'show');
-  addedClass('playerNumber', 'show');
+  addedClass('outcome', 'hide'); // hide the scores box
+  addedClass('turn', 'hide'); // hide the player turn box
+  removedClass('intro', 'hide'); // show the intro box
+  removedClass('playerPiece', 'show'); 
+  addedClass('playerNumber', 'show'); // show screen to pick no. players
   clearBoard();
   tictactoe.playable = false;
 }
 
 // clear the board for the next round
 const clearBoard = () => {
-  console.log('clear the board');
   tictactoe.board = ["", "", "", "", "", "", "", "", ""]
   
   // clear the visual board
@@ -267,10 +207,8 @@ const clearBoard = () => {
   timer = setTimeout(() => {
     addedClass('box', 'show');
     if (tictactoe.noPlayers === 1 && tictactoe.playerTurn === 2){
-      console.log("playing computer");
       setTimeout(computerPlays, 1500);
     } else {
-      console.log("playing person");
       tictactoe.playable = true;
     }
   }, 500);
@@ -285,7 +223,7 @@ const toggleTurns = () => {
   removedClass(`player${currentP}-sign`, 'show');
   addedClass(`player${nextP}-sign`, 'show');
   tictactoe.playerTurn = nextP;
-  console.log(`toggled, it's now player ${nextP} turn`);
+  // console.log(`toggled, it's now player ${nextP} turn`);
   return nextP;
 }
 
@@ -330,10 +268,8 @@ const updateScore = (player, reason) => {
 
 const playOn = () => {
   if (toggleTurns() === 2 && tictactoe.noPlayers === 1){
-    console.log("playing computer");
     timer = setInterval(computerPlays, 1500);
   } else {
-    console.log("player turn");
     tictactoe.playable = true;
   }
 }
@@ -343,7 +279,6 @@ const calculateStatus = () => {
   let winningArr = checkWinner(tictactoe.board);
   if (winningArr) {
     // winner found
-    console.log("We have a WINNER");
     colorWinner(winningArr[0], winningArr[1], winningArr[2]);
     timer = setTimeout(() => {
       removedClass('box', 'show');
@@ -359,7 +294,7 @@ const calculateStatus = () => {
       displayOutcome('draw');
       setTimeout(() => {
         toggleTurns();
-        setTimeout(clearBoard, 1000);
+        clearBoard();
       }, 1500);
     }, 1000);
 
@@ -384,13 +319,10 @@ const boxEmpty = (box) => {
 const markBoard = (box) => {
   document.getElementById(box).innerHTML = tictactoe.players[`player${tictactoe.playerTurn}`]
   tictactoe.board[Number(box[1])] = tictactoe.players[`player${tictactoe.playerTurn}`]
-  console.log(tictactoe.board);
 }
 
 // player has selected a box
 const markBox = (event) => {
-  console.log(event);
-  console.log(event.id);
   if (boxEmpty(event.id)) {
     tictactoe.playable = false;
     markBoard(event.id);
@@ -417,7 +349,6 @@ const startGame = () => {
   removedClass('outcome', 'hide');
   addedClass('intro', 'hide');
   timer = setTimeout(addedClass, 500, `player${tictactoe.playerTurn}-sign`, 'show');
-  console.log(tictactoe);
 }
 
 // set the players' pieces (X or O)
@@ -425,7 +356,6 @@ const setPlayerPiece = (event) => {
   let piece = event.id.charAt(event.id.length-1);
   tictactoe.players.player1 = piece;
   piece === 'X' ? tictactoe.players.player2 = 'O' : tictactoe.players.player2 = 'X'; 
-  console.log(tictactoe.players.player1, tictactoe.players.player2);
   removedClass('playerPiece', 'show');
   timer = setTimeout(() => {
     addedClass('box', 'show');
@@ -446,7 +376,6 @@ const setPlayerNumber = (event) => {
   if (event.id.charAt(event.id.length-1) === '1') {
     updateForComputer();
   }
-  console.log(tictactoe.noPlayers);
   removedClass('playerNumber', 'show');
   timer = setTimeout(addedClass, 1000, 'playerPiece', 'show');
 }
