144424ecb9a33389e38323866ebde728fd767d77
cazyw <cazyw@users.noreply.github.com>
2017-10-28 14:21:33 +1100

    Refactor the game mechanics

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,14 +1,7 @@
-// let players = {
-//   player1: 'X',
-//   player2: 'O'
-// }
-// let turnPlayer = 1
-// let board = ["", "", "", "", "", "", "", "", ""]
-// let winner = ""
-// let playable = false
+
 var timer
 var tictactoe
-// let playerNo = 1;
+
 
 class Game {
   constructor() {
@@ -24,187 +17,50 @@ class Game {
   }
 }
 
-
-const playerTurn = () => {
-  clearTimeout(timer)
-  document.getElementById(`player${turnPlayer}-text`).style.color = 'red'
-  document.getElementById(`player${turnPlayer}-sign`).classList.add('show');
-}
-
-const displayScores = () => {
-  document.getElementsByClassName('outcome')[0].classList.remove('hide')
-  document.getElementsByClassName('turn')[0].classList.remove('hide')
-  document.getElementsByClassName('intro')[0].classList.add('hide')
-
-}
-
-
-
+// highlight the winning row/column/diagonal
 const colorWinner = (a, b, c) => {
   document.getElementById(`b${a}`).style.color = 'rgb(63, 191, 127)'
   document.getElementById(`b${b}`).style.color = 'rgb(63, 191, 127)'
   document.getElementById(`b${c}`).style.color = 'rgb(63, 191, 127)'
 }
 
+// check if there's a winning row/column/diagonal
 const checkWinner = (board) => {
   if (board[0] === board[1] && board[1] === board[2] && board[1] != ""){
     console.log('row 1');
-    // winner = board[0];
-    // colorWinner(0, 1, 2);
     return [0, 1, 2];
   }
   if (board[3] === board[4] && board[4] === board[5] && board[4] != ""){
     console.log('row 2')
-    // winner = board[3]
-    // colorWinner(3, 4, 5)
     return [3, 4, 5];
   }
   if (board[6] === board[7] && board[7] === board[8] && board[7] != ""){
     console.log('row 3')
-    // winner = board[6]
-    // colorWinner(6, 7, 8)
     return [6, 7, 8];
   }
   if (board[0] === board[3] && board[3] === board[6] && board[3] != ""){
     console.log('col 1')
-    // winner = board[0]
-    // colorWinner(0, 3, 6)
     return [0, 3, 6];
   }
   if (board[1] === board[4] && board[4] === board[7] && board[4] != ""){
     console.log('col 2')
-    // winner = board[1]
-    // colorWinner(1, 4, 7)
     return [1, 4, 7];
   }
   if (board[2] === board[5] && board[5] === board[8] && board[5] != ""){
     console.log('col 3')
-    // winner = board[2]
-    // colorWinner(2, 5, 8)
     return [2, 5, 8];
   }
   if (board[0] === board[4] && board[4] === board[8] && board[4] != ""){
     console.log('diag lr')
-    // winner = board[0]
-    // colorWinner(0, 4, 8)
     return [0, 4, 8];
   }
   if (board[2] === board[4] && board[4] === board[6] && board[4] != ""){
     console.log('diag rl')
-    // winner = board[2]
-    // colorWinner(2, 4, 6)
     return [2, 4, 6];
   } 
   return false;
 }
 
-const computerPlays = () => {
-  clearTimeout(timer);
-  let piece = compMove(board);
-  document.getElementById(`b${piece}`).innerHTML = players[`player${turnPlayer}`]
-  board[piece] = players[`player${turnPlayer}`]
-  document.getElementById(`player${turnPlayer}-sign`).classList.remove('show')
-  document.getElementById(`player${turnPlayer}-text`).style.color = 'black' 
-  playable = false
-  console.log(board)
-  calculate()
-
-}
-
-const calculate = (piece) => {
-  let arr = checkWinner(board);
-  if (arr) {
-    winner = arr[0];
-    colorWinner(arr[0], arr[1], arr[2]);
-    document.getElementById(`player${turnPlayer}-text`).style.color = 'black' 
-    document.getElementById(`player${turnPlayer}`).innerHTML = Number(document.getElementById(`player${turnPlayer}`).textContent) + 1
-    document.getElementsByClassName('displayWinner')[0].innerHTML = `Winner:<br/> Player ${turnPlayer}`
-    console.log(`winner: ${turnPlayer}`)
-    playable = false
-    timer = setTimeout(displayOutcome, 500)
-    
-  } else if (board.find(val => val === "") === undefined){
-    document.getElementById(`player${turnPlayer}-text`).style.color = 'black' 
-    document.getElementsByClassName('displayWinner')[0].innerHTML = `Draw`
-    turnPlayer === 1 ? turnPlayer = 2 : turnPlayer = 1
-    playable = false
-    timer = setTimeout(displayOutcome, 500)
-  } else {
-    turnPlayer === 1 ? turnPlayer = 2 : turnPlayer = 1
-    document.getElementById(`player${turnPlayer}-text`).style.color = 'red' 
-    document.getElementById(`player${turnPlayer}-sign`).classList.add('show')
-    if (playerNo === 2 || turnPlayer === 1){
-      playable = true
-    } else {
-      console.log("playing computer");
-      timer = setInterval(computerPlays, 2000);
-    }
-  }
-}
-
-const displayOutcome = () => {
-  document.getElementsByClassName('displayWinner')[0].classList.add('show')
-  clearTimeout(timer)
-  timer = setTimeout(clearBoard, 1500)
-}
-const clearBoard = () => {
-  board = ["", "", "", "", "", "", "", "", ""]
-  for (let i = 0 ; i < 9 ; i++) {
-    document.getElementById(`b${i}`).innerHTML = ""
-    document.getElementById(`b${i}`).style.color = 'rgba(255, 255, 255, 0.5)'
-  }
-  document.getElementById(`player1-text`).style.color = 'black' 
-  document.getElementById(`player2-text`).style.color = 'black' 
-  document.getElementById(`player${turnPlayer}-text`).style.color = 'red'
-  document.getElementById(`player${turnPlayer}-sign`).classList.add('show')
-  winner = ""
-  if (turnPlayer === 1 || playerNo === 2) {
-    playable = true;
-  }
-  document.getElementsByClassName('displayWinner')[0].classList.remove('show')
-  console.log('board cleared')
-  clearTimeout(timer)
-  if (playerNo === 1 && turnPlayer === 2){
-    timer = setInterval(computerPlays, 2000);
-  }
-}
-
-const resetGame = () => {
-  clearInterval(timer);
-  clearTimeout(timer);
-  document.getElementById('player1').innerHTML = 0
-  document.getElementById('player2').innerHTML = 0
-  document.getElementsByClassName('outcome')[0].classList.add('hide')
-  document.getElementsByClassName('turn')[0].classList.add('hide')
-  document.getElementsByClassName('intro')[0].classList.remove('hide')
-  document.getElementsByClassName('playerChoose')[0].classList.remove('game')
-  clearBoard()
-  document.getElementById(`player${turnPlayer}-sign`).classList.remove('show')
-  turnPlayer = 1;
-  playable = false
-}
-
-const markBoard = (event) => {
-  if(tictactoe.playable && document.getElementById(event.id).innerHTML === ""){
-    document.getElementById(event.id).innerHTML = players[`player${turnPlayer}`]
-    board[Number(event.id[1])] = players[`player${turnPlayer}`]
-    document.getElementById(`player${turnPlayer}-sign`).classList.remove('show')
-    document.getElementById(`player${turnPlayer}-text`).style.color = 'black' 
-    playable = false
-    console.log(board)
-    calculate(Number(event.id[1]));
-  }
-}
-
-const game = (event) => {
-  console.log(event.id)
-  markBoard(event)
-}
-
-// const playGame = () => {
-//   console.log("clicked")
-//   document.getElementsByClassName('test')[0].classList.add('game')
-// }
 
 
 // let board = ["", "", "", "", "", "", "", "", ""]
@@ -259,8 +115,8 @@ const randomiseArray = (arr) => {
 }
 
 const compMove = (board) => {
-  var compPiece = players.player2;
-  console.log("COMPUTER: ", players.player2);
+  var compPiece = tictactoe.players.player2;
+  console.log("COMPUTER: ", tictactoe.players.player2);
   var playerPiece = 'X'
   compPiece === 'X' ? playerPiece = 'O' : playerPiece = 'X'; 
   console.log('Board: ', board);
@@ -370,12 +226,188 @@ const compMove = (board) => {
   }
 }
 
-// boards.forEach((board) => {
-//   compMove(board);
-// })
+// computer plays the game;
+const computerPlays = () => {
+  clearTimeout(timer);
+  let piece = compMove(tictactoe.board);
+  var temp = document.createElement('div');
+  temp.id = `b${piece}`;
+  markBox(temp);
+}
 
+// resets the game, doesn't reset the game object values
+// as that is reset on the first menu screen
+const resetGame = () => {
+  console.log('reset game');
+  updateScore('player1', 'reset');
+  updateScore('player2', 'reset');
+
+  addedClass('outcome', 'hide');
+  addedClass('turn', 'hide');
+  removedClass('intro', 'hide');
+  removedClass('playerPiece', 'show');
+  addedClass('playerNumber', 'show');
+  clearBoard();
+  tictactoe.playable = false;
+}
+
+// clear the board for the next round
+const clearBoard = () => {
+  console.log('clear the board');
+  tictactoe.board = ["", "", "", "", "", "", "", "", ""]
+  
+  // clear the visual board
+  for (let i = 0 ; i < 9 ; i++) {
+    document.getElementById(`b${i}`).innerHTML = ""
+    document.getElementById(`b${i}`).style.color = 'rgba(255, 255, 255, 0.5)'
+  }
+  
+  removedClass('displayWinner', 'show');
+  clearTimeout(timer);
+  timer = setTimeout(() => {
+    addedClass('box', 'show');
+    if (tictactoe.noPlayers === 1 && tictactoe.playerTurn === 2){
+      console.log("playing computer");
+      setTimeout(computerPlays, 1500);
+    } else {
+      console.log("playing person");
+      tictactoe.playable = true;
+    }
+  }, 500);
+  
+}
+
+// toggle the turns
 const toggleTurns = () => {
+  let currentP = tictactoe.playerTurn;
+  let nextP;
+  currentP === 1 ? nextP = 2 : nextP = 1;
+  removedClass(`player${currentP}-sign`, 'show');
+  addedClass(`player${nextP}-sign`, 'show');
+  tictactoe.playerTurn = nextP;
+  console.log(`toggled, it's now player ${nextP} turn`);
+  return nextP;
+}
 
+
+// display the outcome screen
+const displayOutcome = (outcome) => {
+
+  // set the display text
+  switch(outcome) {
+    case 'win':
+      if (tictactoe.noPlayers === 1 && tictactoe.playerTurn === 2) {
+        document.getElementsByClassName('displayWinner')[0].innerHTML = `Winner:<br/> Computer`
+      } else {
+        document.getElementsByClassName('displayWinner')[0].innerHTML = `Winner:<br/> Player ${tictactoe.playerTurn}`
+      }
+      break;
+    case 'draw':
+      document.getElementsByClassName('displayWinner')[0].innerHTML = `Draw`;
+      break;
+    default:
+      console.log('error in display outcome switch');
+      break;
+  }
+
+  addedClass('displayWinner','show')
+}
+
+// update the score
+const updateScore = (player, reason) => {
+  switch(reason) {
+    case 'win':
+      document.getElementById(player).innerHTML = Number(document.getElementById(player).textContent) + 1;
+      break;
+    case 'reset':
+      document.getElementById(player).innerHTML = "";
+      break;
+    default:
+      console.log("error in updating the score");
+      break;
+  }
+}
+
+const playOn = () => {
+  if (toggleTurns() === 2 && tictactoe.noPlayers === 1){
+    console.log("playing computer");
+    timer = setInterval(computerPlays, 1500);
+  } else {
+    console.log("player turn");
+    tictactoe.playable = true;
+  }
+}
+
+// calculate if win, draw, continue
+const calculateStatus = () => {
+  let winningArr = checkWinner(tictactoe.board);
+  if (winningArr) {
+    // winner found
+    console.log("We have a WINNER");
+    colorWinner(winningArr[0], winningArr[1], winningArr[2]);
+    timer = setTimeout(() => {
+      removedClass('box', 'show');
+      updateScore(`player${tictactoe.playerTurn}`, 'win');
+      displayOutcome('win');
+      setTimeout(clearBoard, 1500);
+    }, 1000);
+
+  } else if (tictactoe.board.find(val => val === "") === undefined) {
+    // it's a draw
+    console.log("It's a DRAW");
+    timer = setTimeout(() => {
+      displayOutcome('draw');
+      setTimeout(() => {
+        toggleTurns();
+        setTimeout(clearBoard, 1000);
+      }, 1500);
+    }, 1000);
+
+  } else {
+    // continue playing
+    console.log("Play on");
+    playOn();
+  }
+}
+
+// check if the box selected is empty
+const boxEmpty = (box) => {
+  if (tictactoe.noPlayers === 1 && tictactoe.playerTurn === 2) {
+    return document.getElementById(box).innerHTML === "";
+  } else {
+    return tictactoe.playable && document.getElementById(box).innerHTML === "";
+  }
+}
+
+
+// mark the board
+const markBoard = (box) => {
+  document.getElementById(box).innerHTML = tictactoe.players[`player${tictactoe.playerTurn}`]
+  tictactoe.board[Number(box[1])] = tictactoe.players[`player${tictactoe.playerTurn}`]
+  console.log(tictactoe.board);
+}
+
+// player has selected a box
+const markBox = (event) => {
+  console.log(event);
+  console.log(event.id);
+  if (boxEmpty(event.id)) {
+    tictactoe.playable = false;
+    markBoard(event.id);
+    calculateStatus();
+  }
+}
+
+// add the grid outline
+const addedClass = (boxes, cl) => {
+  clearTimeout(timer);
+  [].forEach.call(document.getElementsByClassName(boxes), box => box.classList.add(cl));
+}
+
+// add the grid outline
+const removedClass = (boxes, cl) => {
+  clearTimeout(timer);
+  [].forEach.call(document.getElementsByClassName(boxes), box => box.classList.remove(cl));
 }
 
 // the game is now set and playable
@@ -388,42 +420,35 @@ const startGame = () => {
   console.log(tictactoe);
 }
 
-// these two functions are needed to show/hide screens
-const addedClass = (el, cl) => {
-  clearTimeout(timer);
-  document.getElementById(el).classList.add(cl);
-}
-
-const removedClass = (el, cl) => {
-  clearTimeout(timer);
-  document.getElementById(el).classList.remove(cl);
-}
-
-// add the grid outline
-const addBoxes = (boxes, cl) => {
-  clearTimeout(timer);
-  [].forEach.call(document.getElementsByClassName(boxes), box => box.classList.add(cl));
-  console.log('set the board');
-  startGame();
-}
-
 // set the players' pieces (X or O)
 const setPlayerPiece = (event) => {
   let piece = event.id.charAt(event.id.length-1);
   tictactoe.players.player1 = piece;
   piece === 'X' ? tictactoe.players.player2 = 'O' : tictactoe.players.player2 = 'X'; 
   console.log(tictactoe.players.player1, tictactoe.players.player2);
-  removedClass('playerChoose', 'show');
-  timer = setTimeout(addBoxes, 1000, 'box', 'show');
+  removedClass('playerPiece', 'show');
+  timer = setTimeout(() => {
+    addedClass('box', 'show');
+    startGame();
+  }, 1000);
+}
+
+const updateForComputer = () => {
+  document.getElementById('player2-sign').innerHTML = "Computer's Turn";
+  document.getElementById('player2-text').innerHTML = "Computer: ";
+
 }
 
 // set the number of players (1 or 2)
 const setPlayerNumber = (event) => {
   setupGame();
   tictactoe.noPlayers = Number(event.id.charAt(event.id.length-1));
+  if (event.id.charAt(event.id.length-1) === '1') {
+    updateForComputer();
+  }
   console.log(tictactoe.noPlayers);
   removedClass('playerNumber', 'show');
-  timer = setTimeout(addedClass, 1000, 'playerChoose', 'show');
+  timer = setTimeout(addedClass, 1000, 'playerPiece', 'show');
 }
 
 // create the game object
